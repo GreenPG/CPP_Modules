aacb3eed5b76c47a3c24cdcb8892eb483c24cbd1
Gillian Pasquette <gpasquet@student.42angouleme.fr>
2023-03-21 17:36:35 +0100

    debut PhoneBook

--- CPP_00/ex01/main.cpp
+++ CPP_00/ex01/main.cpp
@@ -1,11 +1,28 @@
 #include "PhoneBook.hpp"
 #include <iostream>
+#include <string>
+
+enum	e_Command_id {ADD, SEARCH, EXIT};
 
 int	main(void)	{
 	PhoneBook	phoneBook;
+	int			input;
 
 	while (1)	{
+		std::cout << "Type a command :";
+		std::cin >> input;
+		if (input != ADD || input != SEARCH || input != EXIT)
+			std::cout << "Only ADD, SEARCH and EXIT are accpeted" << std::endl;
+		else	{
+			switch	(input)	{
+				case (ADD):
 
+				case (SEARCH):
+
+				case (EXIT):
+					break;
+			}
+		}
 	}
 	return (0);
 }
